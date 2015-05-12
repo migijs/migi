ae5c825e2e3e52fbd726fd74dade22ce2348147e
army8735 <army8735@qq.com>
2015-05-12 10:13:25 +0800

    jsx中的js变量分TEXT和VIRTUAL类型记录

--- src/Obj.js
+++ src/Obj.js
@@ -1,6 +1,25 @@
 import VirtualDom from './VirtualDom';
 import util from './util';
 
+function getType(v) {
+  if(Array.isArray(v)) {
+    //递归数组中有一个是VIRTUALDOM都算VIRTUALDOM
+    for(var i = 0, len = v.length; i < len; i++) {
+      var t = getType(v[i]);
+      if(t == Obj.VIRTUALDOM) {
+        return Obj.VIRTUALDOM;
+      }
+    }
+    return Obj.TEXT;
+  }
+  else if(v instanceof VirtualDom) {
+    return Obj.VIRTUALDOM;
+  }
+  else {
+    return Obj.TEXT;
+  }
+}
+
 class Obj {
   constructor(k, context, cb) {
     //fix循环依赖
@@ -11,6 +30,7 @@ class Obj {
     this._context = context;
     this.v = cb.call(context);
     this._cb = cb;
+    this.type = null;
   }
   get k() {
     return this._k;
@@ -22,6 +42,7 @@ class Obj {
     return this._v;
   }
   set v(v) {
+    this.type = getType(v);
     this._v = util.clone(v);
   }
   get cb() {
@@ -31,5 +52,10 @@ class Obj {
     return (this.v || '').toString();
   }
 }
+
+//jsx创建有2种类型：纯文本或js变量返回String或Array<String>都是TEXT；包含VirtualDom则为VIRTUALDOM
+//当Obj作为VirtualDom的child变更时，如果发生类型改变或VIRTUALDOM类型改变，通知parent重绘
+Obj.TEXT = 'TEXT';
+Obj.VIRTUALDOM = 'VIRTUALDOM';
 
 export default Obj;
--- web/Obj.js
+++ web/Obj.js
@@ -1,6 +1,25 @@
 define(function(require, exports, module){var VirtualDom=function(){var _0=require('./VirtualDom');return _0.hasOwnProperty("VirtualDom")?_0.VirtualDom:_0.hasOwnProperty("default")?_0.default:_0}();
 var util=function(){var _1=require('./util');return _1.hasOwnProperty("util")?_1.util:_1.hasOwnProperty("default")?_1.default:_1}();
 
+function getType(v) {
+  if(Array.isArray(v)) {
+    //递归数组中有一个是VIRTUALDOM都算VIRTUALDOM
+    for(var i = 0, len = v.length; i < len; i++) {
+      var t = getType(v[i]);
+      if(t == Obj.VIRTUALDOM) {
+        return Obj.VIRTUALDOM;
+      }
+    }
+    return Obj.TEXT;
+  }
+  else if(v instanceof VirtualDom) {
+    return Obj.VIRTUALDOM;
+  }
+  else {
+    return Obj.TEXT;
+  }
+}
+
 
   function Obj(k, context, cb) {
     //fix循环依赖
@@ -11,6 +30,7 @@ var util=function(){var _1=require('./util');return _1.hasOwnProperty("util")?_1
     this._context = context;
     this.v = cb.call(context);
     this._cb = cb;
+    this.type = null;
   }
   var _2={};_2.k={};_2.k.get =function() {
     return this._k;
@@ -22,6 +42,7 @@ var util=function(){var _1=require('./util');return _1.hasOwnProperty("util")?_1
     return this._v;
   }
   _2.v.set =function(v) {
+    this.type = getType(v);
     this._v = util.clone(v);
   }
   _2.cb={};_2.cb.get =function() {
@@ -31,5 +52,10 @@ var util=function(){var _1=require('./util');return _1.hasOwnProperty("util")?_1
     return (this.v || '').toString();
   }
 Object.keys(_2).forEach(function(k){Object.defineProperty(Obj.prototype,k,_2[k])});
+
+//jsx创建有2种类型：纯文本或js变量返回String或Array<String>都是TEXT；包含VirtualDom则为VIRTUALDOM
+//当Obj作为VirtualDom的child变更时，如果发生类型改变或VIRTUALDOM类型改变，通知parent重绘
+Obj.TEXT = 'TEXT';
+Obj.VIRTUALDOM = 'VIRTUALDOM';
 
 exports.default=Obj;});
