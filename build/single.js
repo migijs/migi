2c52c51e6f7e6926c7f180efc65a4b39acb31be8
army8735 <army8735@qq.com>
2015-05-12 21:20:07 +0800

    miss

---
+++ build/single.js
@@ -0,0 +1,18 @@
+exports.default={
+  'img': true,
+  'meta': true,
+  'link': true,
+  'br': true,
+  'basefont': true,
+  'base': true,
+  'col': true,
+  'embed': true,
+  'frame': true,
+  'hr': true,
+  'input': true,
+  'keygen': true,
+  'area': true,
+  'param': true,
+  'source': true,
+  'track': true
+};
---
+++ web/single.js
@@ -0,0 +1,18 @@
+define(function(require, exports, module){exports.default={
+  'img': true,
+  'meta': true,
+  'link': true,
+  'br': true,
+  'basefont': true,
+  'base': true,
+  'col': true,
+  'embed': true,
+  'frame': true,
+  'hr': true,
+  'input': true,
+  'keygen': true,
+  'area': true,
+  'param': true,
+  'source': true,
+  'track': true
+};});
