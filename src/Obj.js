0acdafd53f98161265a35bc84af896d3808c0c1e
army8735 <army8735@qq.com>
2015-05-15 09:26:28 +0800

    增加COMPLEX类型，以及初始化为空文本节点的empty属性

--- src/Obj.js
+++ src/Obj.js
@@ -1,23 +1,19 @@
 import VirtualDom from './VirtualDom';
 import util from './util';
 
-function getType(v) {
+function getType(v, list) {
   if(Array.isArray(v)) {
-    //递归数组中有一个是VIRTUALDOM都算VIRTUALDOM
-    for(var i = 0, len = v.length; i < len; i++) {
-      var t = getType(v[i]);
-      if(t == Obj.VIRTUALDOM) {
-        return Obj.VIRTUALDOM;
-      }
-    }
-    return Obj.TEXT;
+    v.forEach(function(item) {
+      list.push(getType(item, list));
+    });
   }
   else if(v instanceof VirtualDom) {
-    return Obj.VIRTUALDOM;
+    list.push(Obj.VIRTUALDOM);
   }
   else {
-    return Obj.TEXT;
+    list.push(Obj.TEXT);
   }
+  return list;
 }
 function joinArray(arr, unEscape) {
   var res = '';
@@ -38,27 +34,71 @@ class Obj {
     if(VirtualDom.hasOwnProperty('default')) {
       VirtualDom = VirtualDom.default;
     }
-    this._k = k;
-    this._context = context;
+    this.__k = k;
+    this.__context = context;
+    this.__empty = true;
     this.type = null;
+    this.__count = 0;
+    this.__cb = cb;
     this.v = cb.call(context);
-    this._cb = cb;
   }
   get k() {
-    return this._k;
+    return this.__k;
   }
   get context() {
-    return this._context;
+    return this.__context;
   }
   get v() {
-    return this._v;
+    return this.__v;
   }
   set v(v) {
-    this.type = getType(v);
-    this._v = util.clone(v);
+    var self = this;
+    var list = getType(v, []);
+    //数组只有1项时，为其对应类型；
+    //多项时判断2种数量，全部为一种也是对应类型；否则为COMPLEX类型
+    //count计数为VirtualDom节点数
+    var iV = 0;
+    var iT = 0;
+    list.forEach(function(item) {
+      if(item instanceof VirtualDom) {
+        iV++;
+        self.__count++;
+      }
+      else {
+        iT++;
+      }
+    });
+    if(list.length == 1) {
+      self.type == list[0];
+    }
+    else {
+      if(iV == 0) {
+        self.type == Obj.TEXT;
+      }
+      else if(iT == 0) {
+        self.type == Obj.VIRTUALDOM;
+      }
+      else {
+        self.type == Obj.COMPLEX;
+      }
+    }
+    //只有TEXT类型需要关心empty，因为空字符串初始化时若处于2个DOM之间，则不占文本节点对象，需新建
+    //以后无论如何变更，只要变成非空字符串，都不是empty，因为TextNode已经存在，变为空也无所谓
+    if(self.type == Obj.TEXT) {
+      if(v != '') {
+        self.__empty = false;
+      }
+    }
+    self.__v = util.clone(v);
   }
   get cb() {
-    return this._cb;
+    return this.__cb;
+  }
+  get count() {
+    return this.__count;
+  }
+  get empty() {
+    return this.__empty;
   }
   toString(unEscape) {
     var self = this;
@@ -75,5 +115,6 @@ class Obj {
 //当Obj作为VirtualDom的child变更时，如果发生类型改变或VIRTUALDOM类型改变，通知parent重绘
 Obj.TEXT = 'TEXT';
 Obj.VIRTUALDOM = 'VIRTUALDOM';
+Obj.COMPLEX = 'COMPLEX';
 
 export default Obj;
--- web/Obj.js
+++ web/Obj.js
@@ -1,23 +1,19 @@
 define(function(require, exports, module){var VirtualDom=function(){var _0=require('./VirtualDom');return _0.hasOwnProperty("VirtualDom")?_0.VirtualDom:_0.hasOwnProperty("default")?_0.default:_0}();
 var util=function(){var _1=require('./util');return _1.hasOwnProperty("util")?_1.util:_1.hasOwnProperty("default")?_1.default:_1}();
 
-function getType(v) {
+function getType(v, list) {
   if(Array.isArray(v)) {
-    //递归数组中有一个是VIRTUALDOM都算VIRTUALDOM
-    for(var i = 0, len = v.length; i < len; i++) {
-      var t = getType(v[i]);
-      if(t == Obj.VIRTUALDOM) {
-        return Obj.VIRTUALDOM;
-      }
-    }
-    return Obj.TEXT;
+    v.forEach(function(item) {
+      list.push(getType(item, list));
+    });
   }
   else if(v instanceof VirtualDom) {
-    return Obj.VIRTUALDOM;
+    list.push(Obj.VIRTUALDOM);
   }
   else {
-    return Obj.TEXT;
+    list.push(Obj.TEXT);
   }
+  return list;
 }
 function joinArray(arr, unEscape) {
   var res = '';
@@ -38,27 +34,71 @@ function joinArray(arr, unEscape) {
     if(VirtualDom.hasOwnProperty('default')) {
       VirtualDom = VirtualDom.default;
     }
-    this._k = k;
-    this._context = context;
+    this.__k = k;
+    this.__context = context;
+    this.__empty = true;
     this.type = null;
+    this.__count = 0;
+    this.__cb = cb;
     this.v = cb.call(context);
-    this._cb = cb;
   }
   var _2={};_2.k={};_2.k.get =function() {
-    return this._k;
+    return this.__k;
   }
   _2.context={};_2.context.get =function() {
-    return this._context;
+    return this.__context;
   }
   _2.v={};_2.v.get =function() {
-    return this._v;
+    return this.__v;
   }
   _2.v.set =function(v) {
-    this.type = getType(v);
-    this._v = util.clone(v);
+    var self = this;
+    var list = getType(v, []);
+    //数组只有1项时，为其对应类型；
+    //多项时判断2种数量，全部为一种也是对应类型；否则为COMPLEX类型
+    //count计数为VirtualDom节点数
+    var iV = 0;
+    var iT = 0;
+    list.forEach(function(item) {
+      if(item instanceof VirtualDom) {
+        iV++;
+        self.__count++;
+      }
+      else {
+        iT++;
+      }
+    });
+    if(list.length == 1) {
+      self.type == list[0];
+    }
+    else {
+      if(iV == 0) {
+        self.type == Obj.TEXT;
+      }
+      else if(iT == 0) {
+        self.type == Obj.VIRTUALDOM;
+      }
+      else {
+        self.type == Obj.COMPLEX;
+      }
+    }
+    //只有TEXT类型需要关心empty，因为空字符串初始化时若处于2个DOM之间，则不占文本节点对象，需新建
+    //以后无论如何变更，只要变成非空字符串，都不是empty，因为TextNode已经存在，变为空也无所谓
+    if(self.type == Obj.TEXT) {
+      if(v != '') {
+        self.__empty = false;
+      }
+    }
+    self.__v = util.clone(v);
   }
   _2.cb={};_2.cb.get =function() {
-    return this._cb;
+    return this.__cb;
+  }
+  _2.count={};_2.count.get =function() {
+    return this.__count;
+  }
+  _2.empty={};_2.empty.get =function() {
+    return this.__empty;
   }
   Obj.prototype.toString = function(unEscape) {
     var self = this;
@@ -75,5 +115,6 @@ Object.keys(_2).forEach(function(k){Object.defineProperty(Obj.prototype,k,_2[k])
 //当Obj作为VirtualDom的child变更时，如果发生类型改变或VIRTUALDOM类型改变，通知parent重绘
 Obj.TEXT = 'TEXT';
 Obj.VIRTUALDOM = 'VIRTUALDOM';
+Obj.COMPLEX = 'COMPLEX';
 
 exports.default=Obj;});
