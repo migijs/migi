827c3bad21f23fe464c045df7804c15aeebc7565
army8735 <army8735@qq.com>
2015-06-13 12:42:30 +0800

    encode

---
+++ tests/encode/script.jsx
@@ -0,0 +1,13 @@
+migi.render(
+  <div>
+    <span id="s1">{ '>' }</span>
+    <span id="s2">{ ' ' }</span>
+    <span id="s3">{ '<' }</span>
+    <span id="s4">{ '<div>' }</span>
+    <span id="s5">{ '    ' }</span>
+    <span id="s6">{ '&' }</span>
+    <span id="s7">{ '&amp;' }</span>
+    <span id="s8">{ '"' }</span>
+  </div>,
+  '#test'
+);
---
+++ tests/encode/test.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var fs = require('fs');
+
+module.exports = {
+  'encode': function(browser) {
+    browser
+      .url('file://' + path.join(__dirname, 'index.html'))
+      .waitForElementVisible('body', 1000)
+      .assert.elementPresent('#test span#s1')
+      .assert.containsText('#test span#s1', '>')
+      .assert.elementPresent('#test span#s2')
+      .assert.containsText('#test span#s2', '')
+      .assert.elementPresent('#test span#s3')
+      .assert.containsText('#test span#s3', '<')
+      .assert.elementPresent('#test span#s4')
+      .assert.containsText('#test span#s4', '<div>')
+      .assert.elementPresent('#test span#s5')
+      .assert.containsText('#test span#s5', '')
+      .assert.elementPresent('#test span#s6')
+      .assert.containsText('#test span#s6', '&')
+      .assert.elementPresent('#test span#s7')
+      .assert.containsText('#test span#s7', '&')
+      .assert.elementPresent('#test span#s8')
+      .assert.containsText('#test span#s8', '"')
+      .end();
+  }
+};
