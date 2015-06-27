ac12314fb3a1a409c8aee7af2e6120bfffd8b253
army8735 <army8735@qq.com>
2015-06-27 11:08:00 +0800

    cachecomponent完善test

---
+++ tests/cachecomponent2/test.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var fs = require('fs');
+
+module.exports = {
+  'cachecomponent': function(browser) {
+    browser
+      .url('file://' + path.join(__dirname, 'index.html'))
+      .waitForElementVisible('body', 1000)
+      .assert.elementPresent('#test p')
+      .assert.containsText('#test p', 'count: 100000, 100000')
+      .assert.containsText('#test2', '1')
+      .assert.containsText('#test3', 'count,count2')
+      .end();
+  }
+};
--- tests/cachecomponent3/script.jsx
+++ tests/cachecomponent3/script.jsx
@@ -2,9 +2,11 @@ class Cache extends migi.CacheComponent {
   constructor(...data) {
     super(...data);
     this.i = 0;
+    this.j = 1;
     this.on(migi.Event.DOM, function() {
       for(var i = 0; i < 100000; i++) {
         this.count++;
+        this.count2++;
       }
     });
   }
@@ -14,8 +16,14 @@ class Cache extends migi.CacheComponent {
   set count(v) {
     this.i = v;
   }
+  get count2() {
+    return this.j;
+  }
+  set count2(v) {
+    this.j = v;
+  }
   render() {
-    return <p>count: {this.count}</p>;
+    return <p>count: { Math.max(this.count, this.count2) }</p>;
   }
 }
 
@@ -24,4 +32,7 @@ var count = 0;
 cache.inTo('#test');
 cache.virtualDom.on(migi.Event.DATA, function() {
   document.querySelector('#test2').innerHTML = ++count;
+});
+cache.virtualDom.on(migi.Event.DATA, function(data) {
+  document.querySelector('#test3').innerHTML = data;
 });
