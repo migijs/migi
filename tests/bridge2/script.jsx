c601a8b7844f9ddf279f5db1ffb1872443ecdc24
army8735 <army8735@qq.com>
2015-06-28 13:49:51 +0800

    bridge test

---
+++ tests/bridge2/script.jsx
@@ -0,0 +1,33 @@
+class Component extends migi.CacheComponent {
+  constructor(...data) {
+    super(...data);
+  }
+  get txt() {
+    return this._txt;
+  }
+  set txt(v) {
+    this._txt = v;
+  }
+  render() {
+    return <span>{this.txt}</span>;
+  }
+}
+
+var vd = migi.render(
+  <div>
+    <Component/>
+    <Component/>
+  </div>,
+  '#test'
+);
+
+var cps = vd.findAll(Component);
+
+cps[0].bridge(cps[1], {
+  'txt': 'txt'
+});
+cps[0].bridgeTo(cps[1], {
+  'txt': 'txt'
+});
+
+cps[0].txt = 'txt';
---
+++ tests/bridge3/script.jsx
@@ -0,0 +1,48 @@
+class Component extends migi.Component {
+  constructor(...data) {
+    super(...data);
+  }
+  get txt() {
+    return this._txt;
+  }
+  set txt(v) {
+    this._txt = v;
+  }
+  render() {
+    return <span>{this.txt}</span>;
+  }
+}
+
+class Component2 extends migi.CacheComponent {
+  constructor(...data) {
+    super(...data);
+  }
+  get txt() {
+    return this._txt;
+  }
+  set txt(v) {
+    this._txt = v;
+  }
+  render() {
+    return <span>{this.txt}</span>;
+  }
+}
+
+var vd = migi.render(
+  <div>
+    <Component/>
+    <Component2/>
+  </div>,
+  '#test'
+);
+
+var cps = vd.findAll(migi.Component);
+
+cps[0].bridge(cps[1], {
+  'txt': 'txt'
+});
+cps[0].bridgeTo(cps[1], {
+  'txt': 'txt'
+});
+
+cps[0].txt = 'txt';
