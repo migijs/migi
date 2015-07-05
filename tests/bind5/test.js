0e3278292ca9bf7705a5670f9ce6378f990f1a16
army8735 <army8735@qq.com>
2015-07-05 15:12:42 +0800

    text

--- tests/bind5/test.js
+++ tests/bind5/test.js
@@ -2,7 +2,7 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = {
-  'bridge': function(browser) {
+  'bind': function(browser) {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
