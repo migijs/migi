ec8c9d6ad8d3e679cfd817d03e46bb1d1ec966cc
army8735 <army8735@qq.com>
2015-08-12 10:08:33 +0800

    lie的e取自widow.event

--- build/fixEvent.js
+++ build/fixEvent.js
@@ -1,19 +1,20 @@
-exports["default"]=function(event) {
+exports["default"]=function(e) {
+  e = e || window.event;
   //IE8
-  if(!event.target) {
-    event.target = event.srcElement || document;
-    event.metaKey = !!event.metaKey;
+  if(!e.target) {
+    e.target = e.srcElement || document;
+    e.metaKey = !!e.metaKey;
     //停止冒泡
-    event.stopPropagation = function() {
-      event.cancelBubble = true;
+    e.stopPropagation = function() {
+      e.cancelBubble = true;
     };
     //阻止默认行为
-    event.preventDefault = function() {
+    e.preventDefault = function() {
       e.returnValue = false;
     };
   }
   //chrome23+以上文本节点会相应事件取不到DOM对象
-  if(event.target.nodeType == 3) {
-    event.target = event.target.parentNode;
+  if(e.target.nodeType == 3) {
+    e.target = e.target.parentNode;
   }
 };
