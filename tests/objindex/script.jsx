b7c4127c0cf5325cedfa0934537d967b644fa8c7
army8735 <army8735@qq.com>
2015-11-06 10:19:34 +0800

    add test

---
+++ tests/objindex/script.jsx
@@ -0,0 +1,42 @@
+class Component extends migi.Component {
+  constructor(...data) {
+    super(...data);
+    this.index = 0;
+    this.list = [
+      [1, 2, 3, 4],
+      [5, 6, 7]
+    ];
+  }
+
+  get index() {
+    return this._index || 0;
+  }
+  set index(v) {
+    this._index = v;
+  }
+  click() {
+    this.index++;
+    this.element.querySelector('p.a').innerHTML = this.find('div').element.childNodes.length;
+    this.element.querySelector('p.b').innerHTML = this.find('div').element.innerHTML.replace(/</g, '&lt;');
+  }
+
+  render() {
+    return <div onClick={this.click}>
+      <p class="a"></p>
+      <p class="b">></p>
+      <div>
+        { this.index ? '' : '' }
+        {
+          this.list[this.index].map(function(item) {
+            return <span>{ item }</span>;
+            })
+          }
+      </div>
+    </div>;
+  }
+}
+
+migi.render(
+  <Component/>,
+  '#test'
+);
---
+++ tests/objindex/test.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var fs = require('fs');
+
+module.exports = {
+  'init': function(browser) {
+    browser
+      .url('file://' + path.join(__dirname, 'index.html'))
+      .waitForElementVisible('body', 1000)
+      .click('#test')
+      .assert.containsText('#test p.a', '4')
+      .assert.containsText('#test p.b', '<span migi-uid="3">5</span><span migi-uid="4">6</span><span migi-uid="5">7</span>')
+      .end();
+  }
+};
