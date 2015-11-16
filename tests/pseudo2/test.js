850cbea7348722fa4b2feefbf6d56ef08ebe51ab
army8735 <army8735@qq.com>
2015-11-16 11:58:50 +0800

    fix test

--- tests/pseudo2/test.js
+++ tests/pseudo2/test.js
@@ -7,9 +7,9 @@ module.exports = {
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
       .assert.cssProperty('#test p', 'margin', '20px')
-      .assert.cssProperty('#test #i1', 'margin', '100px')
-      .assert.cssProperty('#test #i2', 'margin', '101px')
-      .assert.cssProperty('#test #i3', 'margin', '102px')
+      .assert.cssProperty('#test [migi-id="i1"]', 'margin', '100px')
+      .assert.cssProperty('#test [migi-id="i2"]', 'margin', '101px')
+      .assert.cssProperty('#test [migi-id="i3"]', 'margin', '102px')
       .end();
   }
 };
