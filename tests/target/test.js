2c1f54fb0abd354dd26ea837598359b2ba88f13e
army8735 <army8735@qq.com>
2016-03-02 11:20:28 +0800

    fix test

--- tests/target/test.js
+++ tests/target/test.js
@@ -6,8 +6,11 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.elementPresent('#test p')
+      .assert.elementPresent('#test p span')
       .click('#test p')
+      .assert.containsText('#test2', 'p')
+      .assert.containsText('#test3', 'p')
+      .click('#test p span')
       .assert.containsText('#test2', 'p')
       .assert.containsText('#test3', 'span')
       .end();
